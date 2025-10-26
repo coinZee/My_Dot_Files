387fdbaebd93f99b2b8019753db0281fc5f9a7ab
coinzee <coinz1112@gmail.com>
2025-10-26 23:49:15 +0630

    simplified i3

--- .config/i3/themes/catppuccin/polybar/scripts/pacman_update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-updates=$(checkupdates 2>/dev/null | wc -l)
-
-if [ "$updates" -gt 0 ]; then
-  echo "%{F#ff5555}$updates%{F-}"  # red if updates
-else
-  echo "%{F#888888}$updates%{F-}"  # grey if no updates
-fi
-
